0b751aaeda63d6a393db72ba4daf0a7e61afd111
Luis Saavedra <nriqu322@gmail.com>
2020-02-24 14:01:25 -0600

    basic loop structure

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,1 +1,14 @@
-puts 'Hello, world!'
+array = [3,5,1,14,4]
+swap = true
+while swap == true
+  i = 0
+  until i == (array.length-1)
+    if array[i] > array [i + 1]
+      array[i], array[i + 1] = array[i + 1], array[i]
+    else
+      swap = false
+    end
+    i += 1
+  end
+end
+puts array
