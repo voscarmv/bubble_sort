ac332b00cdb9f6af6a7b4eff063a5e44781d764e
Oscar Mier <voscarmv@gmail.com>
2020-02-24 14:35:56 -0600

    Linted with rubocop.

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,12 +1,10 @@
-array = [3,5,1,14,4,19,3,2,1,0,99]
-
 def bubble_sort(arr)
   array = arr
   sorted = false
   while sorted == false
     i = 0
     sorted = true
-    until i == (array.length-1)
+    until i == (array.length - 1)
       if array[i] > array [i + 1]
         array[i], array[i + 1] = array[i + 1], array[i]
         sorted = false
@@ -14,29 +12,28 @@ def bubble_sort(arr)
       i += 1
     end
   end
-  return array
+  array
 end
 
-# puts array.join(", ")
-bubble_sort([5, 4, 3, 2, 1])
+bubble_sort([3, 5, 1, 14, 4, 19, 3, 2, 1, 0, 99])
 
 def bubble_sort_by(arr)
-    array = arr
-    sorted = false
-    while sorted == false
-      i = 0
-      sorted = true
-      until i == (array.length-1)
-        if yield(array[i],array[i+1]) > 0
-          array[i], array[i + 1] = array[i + 1], array[i]
-          sorted = false
-        end
-        i += 1
+  array = arr
+  sorted = false
+  while sorted == false
+    i = 0
+    sorted = true
+    until i == (array.length - 1)
+      if yield(array[i], array[i + 1]).positive?
+        array[i], array[i + 1] = array[i + 1], array[i]
+        sorted = false
       end
+      i += 1
     end
-    return array
   end
+  array
+end
 
-bubble_sort_by(["house", "tree", "apples"]) do |left,right|
-    left.length - right.length
+bubble_sort_by %w[house tree apples] do |left, right|
+  left.length - right.length
 end
